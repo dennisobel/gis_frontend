5c4dc7165fbfc81c078cfe1103606fe7b04de2d8
dennisobel <ogembodenis2016@gmail.com>
2023-05-12 12:28:20 +0300

    changes

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
-import Map, { Marker, Popup, NavigationControl, Source, Layer } from 'react-map-gl';
+import Map, { Marker, Popup, NavigationControl, Source, Layer} from 'react-map-gl';
 import { interpolateOranges, interpolateGreens, interpolateReds, interpolateYlOrRd } from 'd3-scale-chromatic';
 import { useSelector } from 'react-redux';
 import Dot from './Dot';
